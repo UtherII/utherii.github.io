62c0182c6f4d9b366679402fe397230d177c1273
POUYENNE VIGNAU Bruno - m025178 <bruno.pouyenne@soprasteria.com>
2019-10-03 22:49:59 +0200

    browsing initial implementation

---
+++ core/ffi/sidebar-items.js
@@ -0,0 +1,1 @@
+initSidebarItems({"enum":[["c_void","Equivalent to C's `void` type when used as a [pointer]."]],"struct":[["VaList","A wrapper for a `va_list`"],["VaListImpl","Basic implementation of a `va_list`."]]});
